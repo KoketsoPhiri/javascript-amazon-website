c6f28b5bbd6ee9112f69d7c496ff8df1061f91c3
KoketsoPhiri <phirikoketso789@gmail.com>
2024-01-12 22:49:06 +0200

    save productsHTML

---
+++ data/cart.js
@@ -0,0 +1,11 @@
+const cart = [
+  {
+    productName: "Black and Gray Athletic Cotton Socks - 6 Pairs" ,
+    quantity : 1
+  },
+  {
+    productName:"Intermediate Size Basketball" ,
+    quantity : 2
+  }
+];
+
